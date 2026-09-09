d08136aa9203e4efa3758938d9db91a99a820945
tobylovescode <awilliams1893@gmail.com>
2026-09-08 23:51:57 -0400

    Delete YummyMotto2.java

--- YummyMotto2.java
+++
@@ -1,9 +0,0 @@
-public class YummyMotto2 
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("***********************************************");
-        System.out.println("* Yummy makes the food that makes it a party. *");
-        System.out.println("***********************************************");
-    }
-}
